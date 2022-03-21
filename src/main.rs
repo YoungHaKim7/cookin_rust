d55d456a37a296023a0e56709eb13ce701d8ecea
YoungHa Kim <ytok1108@aol.com>
2022-03-21 21:38:36 +0900

    Cooking_Rust 002_A note about error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-// generate random numbers within a range
+// A note about error handling
 
-use rand::Rng;
+use error_chain::error_chain;
+use std::net::IpAddr;
+use std::str;
 
-fn main() {
-    let mut rng = rand::thread_rng();
-    println!("Random f64: {} ", rng.gen::<f64>());
+error_chain! {
+    foreign_links {
+        Utf8(std::str::Utf8Error);
+        AddrParse(std::net::AddrParseError);
+    }
 }
+
+fn main() -> Result<()> {
+    let bytes = b"2001:db8::1";
+
+    // Bytes to string.
+    let s = str::from_utf8(bytes)?;
+
+    // String to IP address.
+    let addr: IpAddr = s.parse()?;
+
+    println!("{addr:?}");
+    Ok(())
+}
+
